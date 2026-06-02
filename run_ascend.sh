66102ff6e79593fbbe0f24f99241fc403d5e1cd4
JassehXia <jassehxia@gmail.com>
2026-06-02 16:42:49 -0400

    changed gpus to 4

--- run_ascend.sh
+++ run_ascend.sh
@@ -6,7 +6,7 @@
 #SBATCH --time=01:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --gpus-per-node=8
+#SBATCH --gpus-per-node=4
 #SBATCH --cluster=ascend
 #SBATCH --output=slurm-%j.out
 #SBATCH --account=PAS2699
