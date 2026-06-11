e358e4b204b9a4b69b8cd2106da083991842e695
JassehXia <jassehxia@gmail.com>
2026-06-11 13:45:32 -0400

    fixed read only output path

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,5 @@
 # Point to the HuggingFace cache so Phi-3 is permenently cached
 export HF_HOME=/fs/ess/PAS2699/huggingface_cache
 
-# Enforce the container's working directory since Singularity ignores Docker's WORKDIR
-cd /app
-
 # Run the pipeline with whatever YAML is passed in, using exec to forward signals and quotes to protect paths
-exec python run_pipeline.py "$1"
+exec python /app/run_pipeline.py "$1"
