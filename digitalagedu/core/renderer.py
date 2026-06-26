6a99e910e86ef49aa6d9fb58ae1c5613597c1b01
JassehXia <jassehxia@gmail.com>
2026-06-26 08:44:33 -0400

    file pathing error inside jinja template

--- digitalagedu/core/renderer.py
+++ digitalagedu/core/renderer.py
@@ -1,10 +1,16 @@
+import os
 from jinja2 import Environment, FileSystemLoader
 
 
 class TemplateRenderer:
     def __init__(self):
+        # Get the directory where renderer.py is located
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        # Templates are located at ../templates relative to core/renderer.py
+        template_dir = os.path.join(os.path.dirname(current_dir), "templates")
+        
         self.env = Environment(
-            loader=FileSystemLoader("digitalagedu/templates"),
+            loader=FileSystemLoader(template_dir),
             autoescape=False
         )
 
