5c0fc1227c74af192c094f8be83162190a7e34ed
JassehXia <jassehxia@gmail.com>
2026-08-10 20:05:01 -0400

    edited entrypoint for system-agnostic running

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Point to the HuggingFace cache so Phi-3 is permenently cached
-export HF_HOME=/fs/ess/PAS2699/huggingface_cache
-
 # Run the pipeline with whatever YAML is passed in, using exec to forward signals and quotes to protect paths
 exec python /app/run_pipeline.py "$1"
